5941182f7037aaba97603d8e0697f164ab24dffb
wy2018 <wyglauk@gmail.com>
2019-08-17 16:04:35 +0100

    + comments

--- 17082019.py
+++ 17082019.py
@@ -8,10 +8,11 @@ basic ideas of:
 '''
 
 # OO object oriented
-# in python3 can only use class method access class property?
+# Q: in python3 can only use class method access class property?
 
 class cls(object):
     
+    # 4.5. cls property, cls method; ins property, ins method
     clsProperty = -2
     
     def __init__(self, inputProperty):
@@ -20,6 +21,7 @@ class cls(object):
     def insMethod(self):
         print(self.insProperty)
         
+    # 3. relationship between instances
     @classmethod
     def clsMethod(cls):
         print(cls.clsProperty)
@@ -32,18 +34,19 @@ class cls(object):
 def subFunc():
     cls.clsMethod()
     
-    ins = cls(-3)    # could be anything
+    # 2. relationship between cls and instance
+    ins = cls(-3)
     ins.__init__(3)
     ins.insMethod()
     ins.clsMethod_2(2)    # modify the shared cls property
 
-    
     ins_2 = cls(1)
     ins_2.clsMethod()
     return ins_2.insMethod
 
 
-def parentFunc(inputFunc):
+# 1. method is object in python
+def parentFunc(inputFunc):    
     sf = inputFunc()
     sf()
 
